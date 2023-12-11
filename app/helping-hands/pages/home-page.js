cc37f8b5ea180e368b1f158e257aff9f07827679
Suman-Dhillon <119367545+Suman-Dhillon@users.noreply.github.com>
2023-12-11 07:47:50 -0700

    setup + auth

---
+++ app/helping-hands/pages/home-page.js
@@ -0,0 +1,11 @@
+"use client"
+
+export default function Page() {
+    return (
+        <div>
+        <p>Welcome to Helping Hands</p>
+        
+        <p>Here you can put the designing for the home page</p>
+        </div>
+    );
+}
---
+++ app/helping-hands/pages/profile-form.js
@@ -0,0 +1,10 @@
+"use client"
+
+export default function Profile() {
+    return (
+        <div>
+        <h1>Profile Form</h1>
+        <p>Here you can put the profile form for the user</p>
+        </div>
+    );
+}
