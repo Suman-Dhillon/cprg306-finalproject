d333dec9c84e6cd069edd9ef7a6b678b99487ce0
Suman-Dhillon <119367545+Suman-Dhillon@users.noreply.github.com>
2023-12-15 20:31:48 -0700

    Dashboard updated

--- app/helping-hands/dashboard/dash.js
+++ app/helping-hands/dashboard/dash.js
@@ -5,9 +5,9 @@ export default function DashboardComponent() {
     return (
         <>
             <h1 className="font-bold mb-8 text-center text-4xl m-5">Dashboard</h1>
-            <div className="flex space-x-10">
+            <div className="flex space-x-10 mt-20">
                 <div class="max-w-sm rounded overflow-hidden shadow-lg">
-                    <img class="w-full" src="https://www.shutterstock.com/image-illustration/3d-render-user-avatar-interface-260nw-2351220871.jpg" alt="profile image"/>
+                    <img class="object-cover h-40 w-96" src="https://www.shutterstock.com/image-illustration/3d-render-user-avatar-interface-260nw-2351220871.jpg" alt="profile image"/>
                     <div class="px-6 py-4">
                         <div class="font-bold text-xl mb-2">Profile Form</div>
                         <p class="text-gray-700 text-base">
@@ -23,11 +23,43 @@ export default function DashboardComponent() {
                 </div>
 
                 <div class="max-w-sm rounded overflow-hidden shadow-lg">
-                    <img class="w-full" src="https://us.123rf.com/450wm/kchung/kchung1609/kchung160900599/62327185-learn-more-hanging-sign-3d-illustration-isolated-on-grey-wall.jpg?ver=6" alt="learn more"/> 
+                    <img class="object-contain h-40 w-96" src="https://www.creb.com/-/media/Public/CREBcom/News/CREBNow/2021/March/SocialDonateToDeclutterGetty.jpg?sc_lang=en.jpg" alt="donations"/> 
+                    <div class="px-6 py-4">
+                        <div class="font-bold text-xl mb-2">Donate</div>
+                        <p class="text-gray-700 text-base">
+                        Help the community by donating items you no longer need. Make a difference today.
+                        </p>
+                    </div>
+                    <div class="px-6 pt-4 pb-2">
+                        <Link href="/helping-hands/donate-item"
+                        class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                        Donate Now
+                        </Link>
+                    </div>
+                </div>
+
+                <div class="max-w-sm rounded overflow-hidden shadow-lg">
+                    <img class="object-cover h-40 w-96" src="https://www.shutterstock.com/image-vector/sharing-clothes-people-donation-concept-260nw-1860972010.jpg" alt="donations"/> 
+                    <div class="px-6 py-4">
+                        <div class="font-bold text-xl mb-2">Find Items</div>
+                        <p class="text-gray-700 text-base">
+                        Search the category and the item you need. Contact the donor via email.
+                        </p>
+                    </div>
+                    <div class="px-6 pt-4 pb-2">
+                        <Link href="/helping-hands/Categories"
+                        class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                        Find Items
+                        </Link>
+                    </div>
+                </div>
+
+                <div class="max-w-sm rounded overflow-hidden shadow-lg">
+                    <img class="object-cover h-40 w-96" src="https://us.123rf.com/450wm/kchung/kchung1609/kchung160900599/62327185-learn-more-hanging-sign-3d-illustration-isolated-on-grey-wall.jpg?ver=6" alt="learn more"/> 
                     <div class="px-6 py-4">
                         <div class="font-bold text-xl mb-2">Learn More</div>
                         <p class="text-gray-700 text-base">
-                        If you want to know more about us, our mission, and our vision, click the button below.
+                        If you want to know more about us, our mission, and our vision, please visit.
                         </p>
                     </div>
                     <div class="px-6 pt-4 pb-2">
@@ -38,21 +70,6 @@ export default function DashboardComponent() {
                     </div>
                 </div>
 
-                <div class="max-w-sm rounded overflow-hidden shadow-lg">
-                    <img class="w-full" src="https://static.toiimg.com/thumb/msid-104221732,width-400,resizemode-4/104221732.jpg" alt="donations"/> 
-                    <div class="px-6 py-4">
-                        <div class="font-bold text-xl mb-2">Donate</div>
-                        <p class="text-gray-700 text-base">
-                        Choose the items you want to donate
-                        </p>
-                    </div>
-                    <div class="px-6 pt-4 pb-2">
-                        <Link href="/helping-hands/Categories"
-                        class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-                        Donate Now
-                        </Link>
-                    </div>
-                </div>
             </div>
         </>
     );
