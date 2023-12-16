c4e5ac4756f5b1615bc658eaee691dedfcac7b34
japsimran04 <japsimrankaur.Nanda@edu.sait.ca>
2023-12-15 21:34:15 -0700

    update

--- app/helping-hands/Categories/page.js
+++ app/helping-hands/Categories/page.js
@@ -39,8 +39,9 @@ export default function Categories() {
                 <button className="inline-flex justify-center hover:text-gray-900 items-center py-3 px-5 sm:ms-4 text-base font-medium text-center text-white rounded-lg border border-white hover:bg-gray-100 focus:ring-4 focus:ring-gray-400 mb-5" onClick={() => handleCategorySelect("clothing")}>Clothing</button>
                 <button className="inline-flex justify-center hover:text-gray-900 items-center py-3 px-5 sm:ms-4 text-base font-medium text-center text-white rounded-lg border border-white hover:bg-gray-100 focus:ring-4 focus:ring-gray-400 mb-5" onClick={() => handleCategorySelect("music")}>Music Instruments</button>
                 </div>
-            </section>
+            
             {renderCategoryComponent()}
+            </section>
         </>
     );
 }
